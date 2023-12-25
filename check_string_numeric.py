a98a4e76f2b8b6004c3fb5396dad61aaea9b2c98
NIRANJAN-PATIL-1811 <rosh.rp1811@gmail.com>
2023-12-25 13:12:02 +0530

    Add new program

---
+++ check_string_numeric.py
@@ -0,0 +1,8 @@
+# Write a Python program to check whether a string is numeric.
+
+sample_string = "2a3"
+
+if sample_string.isnumeric():
+    print("True!")
+else:
+    print("The string contains alphabets!")
