25253d3737ac4a808b0632e8973d33c66ba880a9
NIRANJAN-PATIL-1811 <rosh.rp1811@gmail.com>
2024-02-16 20:05:48 +0530

    Add new prorgram

---
+++ remove_keys.py
@@ -0,0 +1,14 @@
+dicA = {
+    'name':'John',
+    'age':30,
+    'address':'usa'
+}
+
+for key in dicA.keys():
+    print(key)
+
+# dicA.pop('age')
+del dicA['age']
+
+for keys in dicA.keys():
+    print(keys)
