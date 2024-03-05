9109e1a6e6904f2f110e43d111ce0845ca0bac84
NIRANJAN PATIL <rosh.rp1811@gmail.com>
2024-03-05 23:35:26 +0530

    Add new program

---
+++ tuple_data.py
@@ -0,0 +1,9 @@
+# Creating a tuple with different data types
+mixed_tuple = (1, "Hello", 3.14, True, [5, 6, 7])
+
+# Displaying the tuple
+print("Mixed Tuple:", mixed_tuple)
+
+# Accessing elements of the tuple
+print("Element at index 1:", mixed_tuple[1])
+print("Element at index 4:", mixed_tuple[4])
