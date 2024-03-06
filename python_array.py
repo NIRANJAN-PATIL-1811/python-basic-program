d41c1cb06835411668608509c0ab7f5851e03b15
NIRANJAN PATIL <rosh.rp1811@gmail.com>
2024-03-06 21:41:40 +0530

    Add new program

---
+++ python_array.py
@@ -0,0 +1,15 @@
+# Define an array (list)
+my_array = [1, 2, 3, 4, 5]
+
+# Function to append a new item to the end of the array
+def append_to_array(array, new_item):
+    array.append(new_item)
+
+# New item to append
+new_item = 6
+
+# Append the new item to the array
+append_to_array(my_array, new_item)
+
+# Print the updated array
+print("Updated Array:", my_array)
