fa23ad03702b9ca55c75ea613c86c1ff21fad1d4
NIRANJAN PATIL <rosh.rp1811@gmail.com>
2024-03-09 20:05:16 +0530

    Add new program

---
+++ colon_of_tuple.py
@@ -0,0 +1,9 @@
+# Using tuple() constructor
+original_tuple = (1, 2, 3, 4)
+tuple_copy = tuple(original_tuple)
+
+# Using slicing notation
+tuple_copy = original_tuple[:]
+
+print("Original Tuple:", original_tuple)
+print("Tuple Copy:", tuple_copy)
